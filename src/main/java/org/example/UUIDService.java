dcd7642b69c3baa80fa669db19a1c4145c4c9cfd
Sluchyk V.V <boss.sluchyk@gmail.com>
2022-08-31 17:51:48 +0300

    Initial commit

---
+++ src/main/java/org/example/UUIDService.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class UUIDService {
+    public List<UUID> getUUIDs(int size) {
+        List<UUID> uuids = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            uuids.add(UUID.randomUUID());
+        }
+        System.out.println("was created");
+        return uuids;
+    }
+}
